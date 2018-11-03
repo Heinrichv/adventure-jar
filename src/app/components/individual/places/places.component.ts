4a274e6a296d74b5171df258a55969b48f77a79c
Heinrich Venter <heini141@outlook.com>
2018-11-03 09:56:04 +0200

    added slider

--- src/app/components/individual/places/places.component.ts
+++ src/app/components/individual/places/places.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnChanges } from '@angular/core';
 import { PlacesService } from 'src/app/services/places.service';
+import { Platform } from '@angular/cdk/platform';
 
 @Component({
   selector: 'app-places',
@@ -7,8 +8,10 @@ import { PlacesService } from 'src/app/services/places.service';
   styleUrls: ['./places.component.scss']
 })
 export class PlacesComponent implements OnInit, OnChanges {
-  constructor(readonly place: PlacesService) { }
-  places: any[];
+  constructor(readonly place: PlacesService, readonly platform: Platform) { }
+
+  places: any[] = [];
+  mobile = this.platform.ANDROID || this.platform.IOS;
   private lat: string = window.localStorage.getItem('lat');
   private long: string = window.localStorage.getItem('long');
 
@@ -30,9 +33,15 @@ export class PlacesComponent implements OnInit, OnChanges {
 
   getPlaces() {
     if (this.lat && this.long) {
+      this.places = [];
       this.place.getNearbyPlaces(this.lat, this.long)
         .subscribe((data: any) => {
-          this.places = data.results.items;
+          const items: any[] = data.results.items;
+          const size = this.mobile ? 1 : 5;
+
+          while (items.length > 0) {
+            this.places.push(items.splice(0, size));
+          }
         });
     }
   }
